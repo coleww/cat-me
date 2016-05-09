bf1ccdc354bf027a97e78046ec02edcff568e598
Jesús Leganés Combarro 'piranna <piranna@gmail.com>
2016-05-09 23:12:37 +0200

    Converted cats definition to JSON

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var cats = require('./cats')
+var cats = require('./cats.json')
+
+for (var name in cats) cats[name] = cats[name].join('\n')
+
 var catNames = Object.keys(cats)
+
 module.exports = function (cat) {
-  if (!cat) cat = catNames[~~(Math.random() * catNames.length)]
+  cat = cat || catNames[~~(Math.random() * catNames.length)]
+
   return cats[cat]
 }
+
 module.exports.catNames = catNames
